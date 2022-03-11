3f75c5c7c4374000bf1d6a945d10ca2bfbaf599d
Simbilt <50386067+Simbilt@users.noreply.github.com>
2022-03-11 16:11:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) =>{
+       switch(e.target.innerText){
+           case 'C':
+               display.innerText = '';
+               break;
+            case '←':
+                if( display.innerText)
+                {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try
+                {
+                    display.innerText = eval(display.innerText);
+                }
+                catch
+                {
+                    display.innerText = "Nauci da koristis digitron";
+                }
+               
+                break;
+           default:
+               display.innerText += e.target.innerText;
+       }
+
+    });
+});
